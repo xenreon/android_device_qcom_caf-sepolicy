cf0a26c6964b112a10903e44121f6b542250cbec
maxwen <max.weninger@gmail.com>
2016-11-19 16:39:37 +0100

    sepolicy: dont include tests policy files
    Change-Id: I4694908e8c123b6909fd75c188a013a17a78bcc7

--- sepolicy.mk
+++ sepolicy.mk
@@ -2,5 +2,4 @@
 BOARD_SEPOLICY_DIRS += \
        device/qcom/sepolicy \
        device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
