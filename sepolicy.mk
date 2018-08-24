e0ecc455556383e1d7f01da798f4a7f261f81765
Marko Man <darkobas@gmail.com>
2018-08-25 00:16:36 +0200

    change sepolicy.mk for P
    Change-Id: Ibebe09b927c43ef0c9c803f40ea73c33c7e07064

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,12 +1,18 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/common \
-    device/qcom/sepolicy/ssg \
-    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy/vendor/common \
+    device/qcom/sepolicy/vendor/ssg \
+    device/qcom/sepolicy/vendor/common/sysmonapp \
+    device/qcom/sepolicy/vendor/$(TARGET_BOARD_PLATFORM)
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/test
+    device/qcom/sepolicy/vendor/test
+endif
+
+ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
+BOARD_SEPOLICY_DIRS += \
+       device/qcom/sepolicy/vendor/ota/$(TARGET_BOARD_PLATFORM)
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
