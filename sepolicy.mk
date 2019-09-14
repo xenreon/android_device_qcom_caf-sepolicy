906ca0e19a6ad87e75ded1dc4945ebd9d1036fad
Marko Man <darkobas@gmail.com>
2019-09-15 01:09:34 +0200

    Android.mk -> sepolicy.mk

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,5 +1,3 @@
-# Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
@@ -58,5 +56,4 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
     BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
     endif
-endif
 endif
